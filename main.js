07ca3b4a5e712028d02a8dda5b5d5c58c0d92b90
Aurelio Melo <aureliomelo003@gmail.com>
2022-09-20 17:52:06 -0500

    all the things made, jut need to fix a few bugs

---
+++ main.js
@@ -0,0 +1,18 @@
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const play = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  play.classList.add('playing')
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+const plays = Array.from(document.querySelectorAll('.play'));
+plays.forEach(play => play.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
